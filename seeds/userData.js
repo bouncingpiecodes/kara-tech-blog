ecc22caee1b63ef1c7aae35d0d84832ad1e47220
“kara <bouncingpie@icloud.com>
2023-02-03 16:51:26 -0500

    added the sequelize require into index.js

---
+++ seeds/userData.js
@@ -0,0 +1,16 @@
+// adding sequelize requires
+const sequelize = require("../config/connection");
+
+const seedUsers = require("./userData");
+const seedPosts = require("./postData");
+const seedComments = require("./commentData");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  await seedUsers();
+  await seedPosts();
+  await seedComments();
+  process.exit(0);
+};
+
+seedDatabase();
