f0af89ca69430fe31d0441bb0479841a9bc56d97
“kara <bouncingpie@icloud.com>
2023-02-05 18:32:15 -0500

    importing the moodel

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,5 @@
+// import the model and
+const router = require("express").Router();
+const { Comment } = require("../../models");
+const sequelize = require("../../config/connection");
+const withAuth = require("../../utils/auth");
