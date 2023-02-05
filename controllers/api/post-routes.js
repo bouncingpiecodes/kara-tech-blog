6306c31d9f158c8965da0defe00d3416867d8f1c
“kara <bouncingpie@icloud.com>
2023-02-05 18:43:28 -0500

    adding the POST route for user session

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -30,3 +30,38 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// GET by id return data when user click specific post id
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+    ],
+  })
+    .then((postData) => {
+      if (!postData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(postData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
