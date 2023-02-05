6c32ed81e35280fbe87b2feed67691c1fa9e26dc
“kara <bouncingpie@icloud.com>
2023-02-05 18:57:26 -0500

    adding GET routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,3 +4,32 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 // imports express' router object
 const router = require("express").Router();
+
+//GET / route find all posts
+router.get("/", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((postData) => {
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
